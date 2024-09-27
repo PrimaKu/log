573d68067626c5369f5f6d86b72f152a4fabde93
Erzha E <erzha@primaku.com>
2024-09-27 23:03:16 +0700

    feat: add context as main params

--- logger.go
+++ logger.go
@@ -10,14 +10,14 @@ import (
 
 type (
 	Logger interface {
-		Debug(msg string, args ...any)
-		Info(msg string, args ...any)
-		Warn(msg string, args ...any)
-		Error(msg string, args ...any)
-		DebugWithFields(msg string, fields map[string]interface{})
-		InfoWithFields(msg string, fields map[string]interface{})
-		WarnWithFields(msg string, fields map[string]interface{})
-		ErrorWithFields(msg string, fields map[string]interface{})
+		Debug(ctx context.Context, msg string, args ...any)
+		Info(ctx context.Context, msg string, args ...any)
+		Warn(ctx context.Context, msg string, args ...any)
+		Error(ctx context.Context, msg string, args ...any)
+		DebugWithFields(ctx context.Context, msg string, fields map[string]interface{})
+		InfoWithFields(ctx context.Context, msg string, fields map[string]interface{})
+		WarnWithFields(ctx context.Context, msg string, fields map[string]interface{})
+		ErrorWithFields(ctx context.Context, msg string, fields map[string]interface{})
 	}
 	logger struct {
 		slog        *slog.Logger
@@ -42,13 +42,13 @@ func NewLogger(serviceName string, level slog.Level, out io.Writer) Logger {
 	return &logger{slog: slogLogger, serviceName: serviceName}
 }
 
-func (l *logger) log(level slog.Level, msg string, args ...any) {
-	l.slog.LogAttrs(context.Background(), level, msg,
+func (l *logger) log(ctx context.Context, level slog.Level, msg string, args ...any) {
+	l.slog.LogAttrs(ctx, level, msg,
 		slog.String("timestamp", time.Now().Format(time.RFC3339)),
 		slog.Any("data", args))
 }
 
-func (l *logger) logWithFields(level slog.Level, msg string, fields map[string]interface{}) {
+func (l *logger) logWithFields(ctx context.Context, level slog.Level, msg string, fields map[string]interface{}) {
 	attrs := make([]slog.Attr, 0, len(fields)+1)
 	attrs = append(attrs, slog.String("timestamp", time.Now().Format(time.RFC3339)))
 
@@ -56,37 +56,37 @@ func (l *logger) logWithFields(level slog.Level, msg string, fields map[string]i
 		attrs = append(attrs, slog.Any(k, v))
 	}
 
-	l.slog.LogAttrs(context.Background(), level, msg, attrs...)
+	l.slog.LogAttrs(ctx, level, msg, attrs...)
 }
 
-func (l *logger) Debug(msg string, args ...any) {
-	l.log(slog.LevelDebug, msg, args...)
+func (l *logger) Debug(ctx context.Context, msg string, args ...any) {
+	l.log(ctx, slog.LevelDebug, msg, args...)
 }
 
-func (l *logger) Info(msg string, args ...any) {
-	l.log(slog.LevelInfo, msg, args...)
+func (l *logger) Info(ctx context.Context, msg string, args ...any) {
+	l.log(ctx, slog.LevelInfo, msg, args...)
 }
 
-func (l *logger) Warn(msg string, args ...any) {
-	l.log(slog.LevelWarn, msg, args...)
+func (l *logger) Warn(ctx context.Context, msg string, args ...any) {
+	l.log(ctx, slog.LevelWarn, msg, args...)
 }
 
-func (l *logger) Error(msg string, args ...any) {
-	l.log(slog.LevelError, msg, args...)
+func (l *logger) Error(ctx context.Context, msg string, args ...any) {
+	l.log(ctx, slog.LevelError, msg, args...)
 }
 
-func (l *logger) DebugWithFields(msg string, fields map[string]interface{}) {
-	l.logWithFields(slog.LevelDebug, msg, fields)
+func (l *logger) DebugWithFields(ctx context.Context, msg string, fields map[string]interface{}) {
+	l.logWithFields(ctx, slog.LevelDebug, msg, fields)
 }
 
-func (l *logger) InfoWithFields(msg string, fields map[string]interface{}) {
-	l.logWithFields(slog.LevelInfo, msg, fields)
+func (l *logger) InfoWithFields(ctx context.Context, msg string, fields map[string]interface{}) {
+	l.logWithFields(ctx, slog.LevelInfo, msg, fields)
 }
 
-func (l *logger) WarnWithFields(msg string, fields map[string]interface{}) {
-	l.logWithFields(slog.LevelWarn, msg, fields)
+func (l *logger) WarnWithFields(ctx context.Context, msg string, fields map[string]interface{}) {
+	l.logWithFields(ctx, slog.LevelWarn, msg, fields)
 }
 
-func (l *logger) ErrorWithFields(msg string, fields map[string]interface{}) {
-	l.logWithFields(slog.LevelError, msg, fields)
+func (l *logger) ErrorWithFields(ctx context.Context, msg string, fields map[string]interface{}) {
+	l.logWithFields(ctx, slog.LevelError, msg, fields)
 }
