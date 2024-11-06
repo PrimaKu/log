81e69ecc5ad4b0d55db4df39955ce8a347a92be2
Erzha E <erzha@primaku.com>
2024-11-06 09:57:29 +0700

    feat: add log with format

--- logger.go
+++ logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log/slog"
 	"os"
@@ -13,10 +14,16 @@ type (
 		Info(ctx context.Context, msg string, args ...any)
 		Warn(ctx context.Context, msg string, args ...any)
 		Error(ctx context.Context, msg string, args ...any)
+
 		DebugWithFields(ctx context.Context, msg string, fields map[string]interface{})
 		InfoWithFields(ctx context.Context, msg string, fields map[string]interface{})
 		WarnWithFields(ctx context.Context, msg string, fields map[string]interface{})
 		ErrorWithFields(ctx context.Context, msg string, fields map[string]interface{})
+
+		Debugf(ctx context.Context, format string, args ...any)
+		Infof(ctx context.Context, format string, args ...any)
+		Warnf(ctx context.Context, format string, args ...any)
+		Errorf(ctx context.Context, format string, args ...any)
 	}
 	logger struct {
 		slog        *slog.Logger
@@ -56,6 +63,11 @@ func (l *logger) logWithFields(ctx context.Context, level slog.Level, msg string
 	l.slog.LogAttrs(ctx, level, msg, attrs...)
 }
 
+func (l *logger) logf(ctx context.Context, level slog.Level, format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	l.slog.LogAttrs(ctx, level, msg)
+}
+
 func (l *logger) Debug(ctx context.Context, msg string, args ...any) {
 	l.log(ctx, slog.LevelDebug, msg, args...)
 }
@@ -86,4 +98,20 @@ func (l *logger) WarnWithFields(ctx context.Context, msg string, fields map[stri
 
 func (l *logger) ErrorWithFields(ctx context.Context, msg string, fields map[string]interface{}) {
 	l.logWithFields(ctx, slog.LevelError, msg, fields)
+}
+
+func (l *logger) Debugf(ctx context.Context, format string, args ...any) {
+	l.logf(ctx, slog.LevelDebug, format, args...)
+}
+
+func (l *logger) Infof(ctx context.Context, format string, args ...any) {
+	l.logf(ctx, slog.LevelInfo, format, args...)
+}
+
+func (l *logger) Warnf(ctx context.Context, format string, args ...any) {
+	l.logf(ctx, slog.LevelWarn, format, args...)
+}
+
+func (l *logger) Errorf(ctx context.Context, format string, args ...any) {
+	l.logf(ctx, slog.LevelError, format, args...)
 }
